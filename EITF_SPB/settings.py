1cfa1c6b2ee77068f1dc744e7b65d54fa4a7a64c
ChmutovAl <chmutov.aleks@yandex.ru>
2025-07-22 13:30:51 +0300

    Update settings.py

--- EITF_SPB/settings.py
+++ EITF_SPB/settings.py
@@ -113,10 +113,10 @@ USE_I18N = True
 USE_TZ = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.timeweb.ru'
-EMAIL_HOST_USER = 'admin@xn----7sbeck8awhclvd8l.xn--p1ai'
-EMAIL_HOST_PASSWORD = 'SWEvcf296!'
-EMAIL_PORT = 2525
+EMAIL_HOST = '*****'
+EMAIL_HOST_USER = '*************'
+EMAIL_HOST_PASSWORD = '*****!'
+EMAIL_PORT = *****
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 SERVER_EMAIL = EMAIL_HOST_USER
